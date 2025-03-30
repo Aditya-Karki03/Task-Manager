c0192e47c15a5f38dc86d8cb4933e91dcd52efee
Aditya Karki <adityakarki201@gmail.com>
2025-03-30 12:54:37 +0530

    Add User and task table

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response } from "express";
+import { PrismaClient } from "../generated/prisma";
 import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
+const prisma = new PrismaClient();
 const port = process.env.PORT || "";
 
 app.get("/", (req: Request, res: Response) => {
